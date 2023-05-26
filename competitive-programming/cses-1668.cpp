58fdb4b00e31a323ebee9b33c51118f072cec8fe
Lucas Ferreira de Almeida <lucasf.almeida.dev@gmail.com>
2023-05-26 15:55:14 -0300

    perf(competitive-programming): Improve cses-1668.cpp

--- competitive-programming/cses-1668.cpp
+++ competitive-programming/cses-1668.cpp
@@ -6,54 +6,45 @@ const int N_MAX = 1e5 + 5;
 const int M_MAX = 2 * 1e5 + 5;
 
 set<int> rels[N_MAX];
-int teams[N_MAX];
 set<int> available;
-set<int> has_team_available;
+queue<int> q;
+int teams[N_MAX];
+bool relaxed[N_MAX];
 
 int n, m;
 
 void solve()
 {
-  int next = 1;
-  teams[next] = 1;
-  has_team_available.insert(next);
+  int cur = 1;
+
+  q.push(cur);
+  teams[cur] = 1;
+
   while (true)
   {
-    // for (auto it = available.begin(); it != available.end(); it++)
-    //   cout << *it << ' ';
-    // cout << endl;
+    cur = q.front();
+    q.pop();
 
-    int next_next = 0;
-
-    for (auto it = rels[next].begin(); it != rels[next].end(); it++)
+    for (auto it = rels[cur].begin(); it != rels[cur].end(); it++)
     {
-      if (teams[next] == teams[*it])
+      if (teams[cur] == teams[*it])
         return;
-      teams[*it] = 3 - teams[next];
-      if (available.find(*it) != available.end())
-      {
-        has_team_available.insert(*it);
-        next_next = *it;
-      }
+      if (!teams[*it])
+        q.push(*it);
+      teams[*it] = 3 - teams[cur];
     }
-    available.erase(next);
-    has_team_available.erase(next);
+
+    relaxed[cur] = true;
+    available.erase(cur);
 
     if (!available.size())
       break;
 
-    next = next_next;
-
-    if (next == 0)
+    if (!q.size())
     {
-      if (has_team_available.size())
-        next = *has_team_available.begin();
-      else
-      {
-        next = *available.begin();
-        teams[next] = 1;
-        has_team_available.insert(next);
-      }
+      cur = *available.begin();
+      q.push(cur);
+      teams[cur] = 1;
     }
   }
 }
